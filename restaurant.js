d020fba6aae8bfda1485a638e0b406ac87ff1155
Alazarf <alazarhailemeskel09@gmail.com>
2022-08-19 03:55:41 -0400

    Done

---
+++ restaurant.js
@@ -0,0 +1,73 @@
+const restaurants = [{
+        "restaurantName": "Marlaina's Mediterranean Kitchen",
+        "address": "643 SW 152nd St, Burien, WA 98166",
+        "lat": 47.608013,
+        "long": -122.335167,
+        "ratings": [{
+                "stars": 4,
+                "comment": "Tasty food coupled with great atmosphere and awesome customer service!!!!"
+            },
+            {
+                "stars": 5,
+                "comment": "Our group had entree's of Salmon, Chicken, Lamb Shank, and Roast."
+            }
+        ]
+    },
+    {
+        "restaurantName": "B-Town Halo Grill",
+        "address": "627 SW 152nd St, Burien, WA 98166",
+        "lat": 47.608013,
+        "long": -122.335167,
+        "ratings": [{
+                "stars": 5,
+                "comment": "Great menu, something for everyone, and friendly service."
+            },
+            {
+                "stars": 5,
+                "comment": "OMG, great new place and we are in heaven."
+            }
+        ]
+    },
+    {
+        "restaurantName": "Lido Restaurant",
+        "address": "13028 Interurban Ave S suit 100, Tukwila, WA 98168",
+        "lat": 47.608013,
+        "long": -122.335167,
+        "ratings": [{
+                "stars": 3,
+                "comment": "Ordered the roasted lamb plate!"
+            },
+            {
+                "stars": 4,
+                "comment": "Very beautiful and clean location and awesome customers services"
+            },
+            {
+                "stars": 4,
+                "comment": "I really enjoyed their service and food"
+            },
+            {
+                "stars": 3,
+                "comment": "I have never had African food before I stumbled into this place. I was taken back as it used to be a deli before the store changed"
+            }
+        ]
+    },
+    {
+        "restaurantName": "19987 1st Ave S Suite 101, Normandy Park, WA 98148",
+        "address": "Schiffbauerdamm 6<br/>Mitte, 10117 Berlin",
+        "lat": 47.608013,
+        "long": -122.335167,
+        "ratings": [{
+                "stars": 5,
+                "comment": "Pasta with italian sausages and wild mushrooms in spicy pink sauce."
+            },
+            {
+                "stars": 5,
+                "comment": "Excellent Food and Great Service with private dining room for out meetings!"
+            },
+            {
+                "stars": 4,
+                "comment": "Excellent food and service, extensive wine list, not cheap but worth the prices."
+            }
+        ]
+    }
+]
